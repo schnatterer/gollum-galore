277185971f0e3a5c5eb152e59cbf8762abc98249
Johannes Schnatterer <johannes@schnatterer.info>
2019-04-09 17:30:58 +0200

    startup.sh: Apply bash best practices.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+set -o errexit -o nounset -o pipefail
+
 # Make sure wiki folder is always initialized, also on mounted volumes
 (git init /gollum/wiki)&
 # Set ENV only for caddy process. Makes it read the Caddyfile and store its other files in /app
-(HOME=/app caddy $CADDY_PARAMS)&
+(HOME=/app caddy ${CADDY_PARAMS})&
 # Start gollum in the foreground
-exec gollum /gollum/wiki $GOLLUM_PARAMS
+exec gollum /gollum/wiki ${GOLLUM_PARAMS}
